3752466bc6771aa4f1b315e4bb53033a3cf095ce
Tamar Al-Tahan <tamaraltahan@gmail.com>
2021-03-16 23:29:29 -0700

    Update DotaAPI.js

--- DotaAPI.js
+++ DotaAPI.js
@@ -1,5 +1,49 @@
-require('dotenv').config();
+//API: https://docs.opendota.com/#section/Introduction
 
-var key = process.env.API_KEY;
+const fetch = require('node-fetch');
+var matchIDList = [];
 
-console.log(key)
+var options = {
+    'method': 'GET',
+    'headers': {
+      'Accept': 'application/json'
+    }
+  };
+
+  //gets recent match history
+async function GetMatchData(){
+  const response = await fetch('https://api.opendota.com/api/players/1986753/recentMatches', options);
+  return response.json();
+}
+
+
+function printData(data){
+    console.log(data)
+}
+
+function printArray(arr){
+    console.log("Array Length:" + arr.length)
+    arr.forEach(function(entry) {
+        console.log(entry);
+      });
+}
+
+//get match IDs from recent matches
+function parseMatchIDs(data){
+    data.forEach(element => {
+        matchIDList.push(element.match_id);
+    });
+}
+
+
+function ParsePlayerIDs(data){
+
+}
+
+GetMatchData().then(data => {
+    printData(data)
+    parseMatchIDs(data)
+    printArray(matchIDList)
+})
+
+
