7d667b6c651f7c6b9a7b1158b8a1f6302ce86e00
Tamar Al-Tahan <tamaraltahan@gmail.com>
2021-03-21 23:56:20 -0700

    Update DotaAPI.js

--- DotaAPI.js
+++ DotaAPI.js
@@ -1,41 +1,59 @@
 //API: https://docs.opendota.com/#section/Introduction
 const myID = 1986753
 const fetch = require('node-fetch');
-var matchIDList = [];
-var playerIDList = [];
+let matchIDList = [];
+let matchDetailsList = []
+let playerIDList = [];
+let WL = []
 
-var options = {
+const options = {
     'method': 'GET',
     'headers': {
-      'Accept': 'application/json'
+        'Accept': 'application/json'
     }
-  };
+};
 
-  //gets recent match history
-  async function GetMatchData(){
+async function sleep(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+
+//gets recent match history
+async function GetMatchData() {
     const response = await fetch('https://api.opendota.com/api/players/' + myID + '/recentMatches', options);
     return response.json();
-  }
-  
-  //get details from a single match
-  async function getMatchDetails(match_id){
+}
+
+//get details from a single match
+async function getMatchDetails(match_id) {
     const response = await fetch('https://api.opendota.com/api/matches/' + match_id, options)
     return response.json();
-  }
+}
 
-function printData(data){
+async function getPlayerWinrate(){
+    console.log("Sleeping for 15 sec...")
+    await sleep(15000);
+    console.log("15 seconds is over")
+    for (const PID of playerIDList) {
+        const response = await fetch(`https://api.opendota.com/api/players/${PID}/wl?lobby_type=7`, options)
+        const data = await response.json()
+        WL.push(data)
+    }
+    return WL
+}
+
+function printData(data) {
     console.log(data)
 }
 
-function printArray(arr){
+function printArray(arr) {
     console.log("Array Length:" + arr.length)
     arr.forEach(function(entry) {
         console.log(entry);
-      });
+    });
 }
 
 //get match IDs from recent matches
-function parseMatchIDs(data){
+function parseMatchIDs(data) {
     data.forEach(element => {
         matchIDList.push(element.match_id);
     })
@@ -43,17 +61,28 @@ function parseMatchIDs(data){
 
 
 //get player IDs from a match
-function ParsePlayerIDs(){
-  matchIDList.forEach(element => {
-    getMatchDetails(element)
-  }
-    )
+async function ParsePlayerIDs() {
+    for (const match of matchIDList) {
+        const matchDetails = await getMatchDetails(match)
+        matchDetailsList.push(matchDetails)
+    }
+
+    matchDetailsList.forEach(element => {
+        const players = element.players
+        players.forEach(player => {
+            const pID = player.account_id
+            if (pID && pID !== myID) playerIDList.push(pID);
+        })
+    })
+
+    return playerIDList
 }
 
 GetMatchData().then(data => {
-    printData(data)
+    //printData(data)
     parseMatchIDs(data)
     printArray(matchIDList)
+    ParsePlayerIDs()
+    .then(() => getPlayerWinrate())
+    .then(() => console.log(WL))
 })
-
-
